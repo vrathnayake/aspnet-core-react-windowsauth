07595587dbfe0621ffb897f1297db07488bd9cb4
vindi <vrathnayake@gmail.com>
2019-04-14 09:48:00 -0700

    Change the template path to our cshtml template

--- Pages/React/config/paths.js
+++ Pages/React/config/paths.js
@@ -71,7 +71,7 @@ module.exports = {
   appPath: resolveApp('.'),
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
+  appHtml: resolveApp('index.template.cshtml'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
